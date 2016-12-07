ee365e2dfec2eb50d78dbd4e25348d52533f1515
Stephen Anderson <sa3196a@student.american.edu>
2016-12-06 17:16:26 -0800

    did some TODOS

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -69,17 +69,23 @@
     // NOTE: Grab the word count from each article body.
       return article.body.split(' ').length;
     })
-    // TODO: complete this reduce to get a grand total word count
-    .reduce(function() {
+    // TODONE: complete this reduce to get a grand total word count
+    .reduce(function(current, previous) {
+      return current + previous;
     });
   };
 
   /* Chain together a `map` and a `reduce` call to
           produce an array of *unique* author names. */
   Article.allAuthors = function() {
+    return Article.allArticles.map(function(article) {
+      return article.author;
+    })
   //return       TODO: map our collection
     //return    TODO: return just the author names
-
+    .filter(function(ele, index, array) {
+      return array.indexOf(ele) === index;
+    });
   /* TODO: For our reduce that we'll chain here -- since we are trying to
       return an array, we'll need to specify an accumulator type (AKA initial value)
       What should this accumulator be and where is it placed? */
